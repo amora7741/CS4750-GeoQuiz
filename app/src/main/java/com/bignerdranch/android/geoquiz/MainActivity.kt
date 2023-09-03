6f4b177b45e868c8cb572b8d723a46c4a150673c
amora7741 <mora7741@gmail.com>
2023-09-02 21:33:43 -0700

    Added toast functionality to buttons

--- app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -2,7 +2,9 @@ package com.bignerdranch.android.geoquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,6 +17,13 @@ class MainActivity : AppCompatActivity() {
 
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
+
+        trueButton.setOnClickListener{view : View ->
+            Toast.makeText(this, R.string.correct_toast,Toast.LENGTH_SHORT).show()
+        }
+        falseButton.setOnClickListener{view : View ->
+            Toast.makeText(this, R.string.incorrect_toast,Toast.LENGTH_SHORT).show()
+        }
 
     }
 }
