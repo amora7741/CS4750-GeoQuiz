464ecd89f30166e025e13a885632b4de4ada7d5e
amora7741 <mora7741@gmail.com>
2023-09-07 11:51:03 -0700

    Remove unused imports

--- app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -1,11 +1,9 @@
 package com.bignerdranch.android.geoquiz
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.Button
 import android.widget.Toast
 import androidx.activity.viewModels
 import com.bignerdranch.android.geoquiz.databinding.ActivityMainBinding
